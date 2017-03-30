05f784ff805060950065f95c084e0cbcb8e1a952
nalcus <nalcus@gmail.com>
2017-03-30 09:44:12 +0000

    first working version

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
 var express = require('express')
+var dateFormat = require('dateformat');
+var app = express()
+app.use(function(req, res) {
+    var str=req.path
+    
+    while(str.charAt(0) === '/')
+    {
+     str = str.substr(1);
+    }
+    
+    if (isNaN(str)) {
+        str=decodeURI(str)
+        var userDate = new Date(str)
+    } else {
+        var userDate = new Date(1000*parseInt(str))
+    }
+    
+    var timestamp = {unix:null,natural:null}
+
+    var valid = (userDate.getTime() > 0);
+    
+    if (valid) {
+     timestamp["unix"]=userDate.getTime()/1000
+     timestamp["natural"]=dateFormat(userDate,"longDate")
+    }
+    
+    
+    
+    res.json(timestamp)
+})
+app.listen(8080)
