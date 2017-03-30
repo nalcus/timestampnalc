195c2135bb7a7a10d653f3a54783be75baf67152
nalcus <nalcus@gmail.com>
2017-03-30 10:55:32 +0000

    fix port issue

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var dateFormat = require('dateformat');
 var app = express()
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(function(req, res) {
     var str=req.path
     
@@ -29,4 +32,7 @@ app.use(function(req, res) {
     
     res.json(timestamp)
 })
-app.listen(8080)
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
